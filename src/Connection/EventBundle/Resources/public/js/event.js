480de86f1975fdacc986048ba00f2762f8ea573e
Jura S <stack@land.ru>
2014-08-15 18:07:45 +0300

    - Event view share button

--- src/Connection/EventBundle/Resources/public/js/event.js
+++ src/Connection/EventBundle/Resources/public/js/event.js
@@ -109,6 +109,19 @@ EM = {
         'hideOverlay': function() {
             $("#overlay").remove();
         }
+    },
+    'social': {
+        'share': {
+            'facebook': function() {
+                (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&appId=333611596791648&version=v2.0";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            }
+        }
     }
 };
 
