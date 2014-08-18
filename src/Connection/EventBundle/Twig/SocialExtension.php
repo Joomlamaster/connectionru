6202f36f5aaed6e4c4ad72f5482500372c5e4cef
Jura S <stack@land.ru>
2014-08-18 15:20:00 +0300

    - Pinterest

--- src/Connection/EventBundle/Twig/SocialExtension.php
+++ src/Connection/EventBundle/Twig/SocialExtension.php
@@ -11,9 +11,13 @@ class SocialExtension extends \Twig_Extension
         );
     }
 
-    public function pinterestShareButton($url, $img)
+    public function pinterestShareButton($url, $img, $description = "ConnectionRu Event")
     {
-        return "<a href='//www.pinterest.com/pin/create/button/?url={$url}&media={$img}&description=ConnectionRu Event' data-pin-do='buttonPin' data-pin-config='none'><img src='//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png' /></a>";
+        $img    = urlencode(!empty($img) ? $img : "http://farm8.staticflickr.com/7027/6851755809_df5b2051c9_z.jpg");
+        $url    = urlencode($url);
+        $desc   = urlencode($description);
+        $link   = "<a href='//www.pinterest.com/pin/create/button/?url={$url}&media={$img}&description={$desc}' data-pin-do='buttonPin' data-pin-config='beside'><img src='//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png' /></a>";
+        return $link;
     }
 
     public function getName()
