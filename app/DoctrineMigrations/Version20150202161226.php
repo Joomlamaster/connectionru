fdfcdec6b753df59eade2908e14e5cb5707e2f7e
leprechaun <leprechaunxx@gmail.com>
2015-02-02 16:40:07 +0200

    Add Singles to event categories

--- app/DoctrineMigrations/Version20150202161226.php
+++ app/DoctrineMigrations/Version20150202161226.php
@@ -14,11 +14,13 @@ class Version20150202161226 extends AbstractMigration
     {
         $this->addSql("UPDATE user_drinking SET name = 'Socially' WHERE name = 'Casually'");
         $this->addSql("INSERT INTO user_profession (name) VALUES ('Transportation')");
+        $this->addSql("INSERT INTO event_category (name) VALUES ('Singles')");
     }
 
     public function down(Schema $schema)
     {
         $this->addSql("UPDATE user_drinking SET name = 'Casually' WHERE name = 'Socially'");
         $this->addSql("DELETE FROM user_profession WHERE name LIKE 'Transportation'");
+        $this->addSql("DELETE FROM event_category WHERE name LIKE 'Singles'");
     }
 }
